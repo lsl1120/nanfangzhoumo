acf07ef5f07fe099f5bc247da35548b591290017
lsl1120 <984745509@qq.com>
2018-12-12 16:59:14 +0800

    完成1.0

---
+++ build/precache-manifest.0c1f9519c29dd98cfb7dcfdebd7f1351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23417c4422ea204ce3cf6ba6b4e27620",
+    "url": "/static/media/7827303_221233267358_2.23417c44.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c71f1a49dd514c99de8e",
+    "url": "/static/js/main.c71f1a49.chunk.js"
+  },
+  {
+    "revision": "83ba694077ad38c55ee7",
+    "url": "/static/js/1.83ba6940.chunk.js"
+  },
+  {
+    "revision": "c71f1a49dd514c99de8e",
+    "url": "/static/css/main.51d4a550.chunk.css"
+  },
+  {
+    "revision": "83ba694077ad38c55ee7",
+    "url": "/static/css/1.ca40903f.chunk.css"
+  },
+  {
+    "revision": "1345289c4c2aff0505436208db13abf0",
+    "url": "/index.html"
+  }
+];
